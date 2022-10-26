3a10c79b21eddb6dac312d93c79a9feffa7dccd2
Himandri Sharma <himandrisharma27@gmail.com>
2022-10-26 12:05:49 +0530

    initial skeleton ready

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,8 +2,8 @@ import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 
 export default class IndexRoute extends Route {
-  @service store
+  @service store;
   async model() {
-    return this.store.findAll('product')
+    return this.store.findAll('product');
   }
 }
