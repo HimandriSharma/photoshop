17e2a2ea8906fde8a15b00b359259229befc56dd
Himandri Sharma <himandrisharma27@gmail.com>
2022-10-28 19:53:41 +0530

    grid integration completed

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,7 +3,24 @@ import { inject as service } from '@ember/service';
 
 export default class IndexRoute extends Route {
   @service store;
+  transform(arr) {
+    var result = [],
+      temp = [];
+    arr.forEach(function (elem, i) {
+      if (i > 0 && i % 3 === 0) {
+        result.push(temp);
+        temp = [];
+      }
+      temp.push(elem);
+    });
+    if (temp.length > 0) {
+      result.push(temp);
+    }
+    return result;
+  }
   async model() {
-    return this.store.findAll('list');
+    const data = await this.store.findAll('list');
+    const res = this.transform(data);
+    return res;
   }
 }
