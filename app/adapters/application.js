fc4a055a356655485d6d6df99fe7d619ff34939a
Himandri Sharma <himandrisharma27@gmail.com>
2022-10-30 10:15:17 +0530

    removed adaptors and serializers

--- app/adapters/application.js
+++
@@ -1,9 +0,0 @@
-import RESTAdapter from '@ember-data/adapter/rest';
-
-export default class ApplicationAdapter extends RESTAdapter {
-  host = 'https://picsum.photos';
-  namespace = 'v2';
-  pathForType() {
-    return 'list';
-  }
-}
